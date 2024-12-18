7bfd5e69f874b611818e97ad8159b190ee4ecd9a
Thomas Caneday <110267727+ThomasCaneday@users.noreply.github.com>
2024-12-17 19:07:28 -0800

    Create config.py
    Added urls for Alpaca and placeholders for api and secret keys

---
+++ config.py
@@ -0,0 +1,5 @@
+BASE_URL = "https://paper-api.alpaca.markets/v2"
+ACCOUNT_URL = "{}/account".format(BASE_URL)
+ORDERS_URL = "{}/orders".format(BASE_URL)
+API_KEY = "your_alpaca_api_key_here"
+SECRET_KEY = "your_alpaca_secret_key_here"
