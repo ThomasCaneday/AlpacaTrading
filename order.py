b95268650dfac15aa84dbffd09b54c65bfce7434
Thomas Caneday <110267727+ThomasCaneday@users.noreply.github.com>
2024-12-18 13:51:01 -0800

    Create order.py
    Added another file for submitting a buy order

---
+++ order.py
@@ -0,0 +1,32 @@
+from alpaca.trading.client import TradingClient
+from alpaca.trading.requests import MarketOrderRequest
+from alpaca.trading.enums import OrderSide, TimeInForce
+
+trading_client = TradingClient('api-key', 'secret-key', paper=True)
+
+# preparing market order
+market_order_data = MarketOrderRequest(
+                    symbol="SPY",
+                    qty=0.023,
+                    side=OrderSide.BUY,
+                    time_in_force=TimeInForce.DAY
+                    )
+
+# Market order
+market_order = trading_client.submit_order(
+                order_data=market_order_data
+               )
+
+# preparing limit order
+limit_order_data = LimitOrderRequest(
+                    symbol="BTC/USD",
+                    limit_price=17000,
+                    notional=4000,
+                    side=OrderSide.SELL,
+                    time_in_force=TimeInForce.FOK
+                   )
+
+# Limit order
+limit_order = trading_client.submit_order(
+                order_data=limit_order_data
+              )
